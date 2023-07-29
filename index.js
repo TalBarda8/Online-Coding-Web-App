a24018780cb53c4d242c77e465128c5698f62332
Tal Barda <talbarda8@gmail.com>
2023-07-29 20:26:31 +0300

    Updating PORT

--- index.js
+++ index.js
@@ -100,7 +100,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
-
